a781cea6c4021955ef02ac52a8bf9e1a603ff189
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-10-31 22:34:27 -0300

    Working with REST APIs - GET Posts Endpoint was created

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Acces-Control-Allow-Origin", "*");
-    res.setHeader("Acces-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Acces-Control-Alow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Alow-Headers", "Content-Type, Authorization");
     next();
 })
 
