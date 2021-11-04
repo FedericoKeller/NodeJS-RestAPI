ce82b48bfc09eb3e8868bac9ed6c0d1371e1f4af
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-11-03 23:03:16 -0300

    Working with Rest APIs -  Fetching and Posting Data

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const DATABASE_CONNECTION = require("./database/database").DATABASE_CONNECTION;
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/images", express.static(path.join(__dirname, "images")))
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE");
@@ -15,5 +19,16 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
 
-app.listen(4200);
+    res.status(status).json({
+        message: message
+    })
+})
+
+mongoose.connect(DATABASE_CONNECTION).then(() => {
+    app.listen(8080);
+})
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,43 +1,86 @@
 const { validationResult } = require("express-validator");
 
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
+  Post.find()
+    .then(posts => {
+        res.status(200).json({
+            message: "Fetched posts succesfully.",
+            posts: posts,
+        })
 
-    res.status(200).json({
-        posts: [{
-            _id: "1",
-            title: "First Post",
-            content: "This is the first post!",
-            imageUrl: "images/boat.jpg",
-            creator: {
-                name: 'Federico',
-            },
-            createdAt: new Date()
-        }],
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
     });
 };
 
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ message: "Validation failed, entered data is incorrect.", errors: errors.array() });
-    }
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
 
-    const title = req.body.title;
-    const content = req.body.content;
+  const title = req.body.title;
+  const content = req.body.content;
 
-    //Create post in db
-    res.status(201).json({
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/boat.jpg",
+    creator: {
+      name: "Federico",
+    },
+  });
+
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      //Create post in db
+      res.status(201).json({
         message: "Post created succesfully!",
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: "Federico",
-            },
-            createdAt: new Date()
-        },
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not  find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.status(200).json({
+        message: "Post fetched.",
+        post: post,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+
+      next(err);
     });
 };
