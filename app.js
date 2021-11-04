4db9597cf25584f76ec2df2da812480caa6cf427
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-11-03 23:35:30 -0300

    Working with REST APIs - Uploading images

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const DATABASE_CONNECTION = require("./database/database").DATABASE_CONNECTION;
 
@@ -8,27 +10,54 @@ const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "images")))
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/feed", feedRoutes);
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
 
-    res.status(status).json({
-        message: message
-    })
-})
+  res.status(status).json({
+    message: message,
+  });
+});
 
 mongoose.connect(DATABASE_CONNECTION).then(() => {
-    app.listen(8080);
-})
+  app.listen(8080);
+});
