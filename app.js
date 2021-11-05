6954b0709d7967ef1f42c8f414796534c1f95063
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-11-04 22:55:18 -0300

    Working with REST APIs - Adding User Model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { v4: uuidv4 } = require("uuid");
 const DATABASE_CONNECTION = require("./database/database").DATABASE_CONNECTION;
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -47,14 +48,18 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
+  const data = error.data;
 
   res.status(status).json({
     message: message,
+    data: data,
   });
 });
 
